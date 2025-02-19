354dc298a3127847af5c62621ff166d3f6f547f8
Niklas <niklaspein12@gmail.com>
2025-02-19 14:05:19 +0100

    fix scroll issues

--- app/src/main/java/com/gewuerz/pacer/Main.kt
+++ app/src/main/java/com/gewuerz/pacer/Main.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -30,7 +32,8 @@ fun Main(client: HealthConnectClient? = null, pref: SharedPreferences? = null, w
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp),
+                        .padding(16.dp)
+                        .verticalScroll(rememberScrollState()),
                     verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
                     TodaySteps(client, pref, packageManager)
